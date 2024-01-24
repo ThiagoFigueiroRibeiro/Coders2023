1232ae30166321c743eb606bff9cfb4ee3b36557
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-01-24 16:49:43 -0300

    Update several files

--- src/main/java/br/ada/exerc/agendaMainPhi.java
+++ src/main/java/br/ada/exerc/agendaMainPhi.java
@@ -1,16 +1,20 @@
 package main.java.br.ada.exerc;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class agendaMainPhi {
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) {
         program();
     }
 
     public static class Contato {
-        private Long id;
+        private final Long id;
         private String nome;
         private String sobreNome;
         private long telefones;
@@ -25,9 +29,9 @@ public class agendaMainPhi {
             return id;
         }
 
-        public void setId(long id) {
-            this.id = id;
-        }
+        //public void setId(long id) {
+        //    this.id = id;
+        //}
 
         public String getNome() {
             return nome;
@@ -64,16 +68,20 @@ public class agendaMainPhi {
             }
             System.out.println();
         }
+
     }
 
-    public class Telefone {
-        private Long id;
-        private String ddd;
-        private Long numero;
+    //public class Telefone {
+        //private Long id;
+        //private String ddd;
+        //private Long numero;
+    //}
+    public static void telJaCadastrado(){
+        System.out.println(ANSI_YELLOW + "ATENÇÃO! Operação cancelada - Telefone já cadastrado." + ANSI_RESET);
     }
 
     static void adicionarContato(List<Contato> contatos, long id) {
-
+        boolean jaCadastrado = false;
         Scanner sc = new Scanner(System.in);
         System.out.print("Escreva o nome do usuário: ");
         String nome = sc.nextLine();
@@ -81,11 +89,25 @@ public class agendaMainPhi {
         String sobrenome = sc.nextLine();
         System.out.print("Escreva o telefone do usuário: ");
         Long telefone = sc.nextLong();
-        contatos.add(new Contato(id, nome, sobrenome, telefone));
+
+        for(Contato i: contatos){
+            if(i.telefones == telefone){
+                jaCadastrado = true;
+                break;
+            }
+        }
+
+        if(jaCadastrado){
+            telJaCadastrado();
+        }
+        else {
+            contatos.add(new Contato(id, nome, sobrenome, telefone));
+
+        }
         Contato.printAllContacts(contatos);
     }
 
-    static void removerContato(List<Contato> contatos, long id) {
+    static void removerContato(List<Contato> contatos) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Escreva o ID do usuário que você deseja deletar: ");
         Long myId = sc.nextLong();
@@ -93,7 +115,8 @@ public class agendaMainPhi {
         Contato.printAllContacts(contatos);
     }
 
-    static void editarContato(List<Contato> contatos, long id) {
+    static void editarContato(List<Contato> contatos) {
+        boolean jaCadastrado = false;
         Scanner sc = new Scanner(System.in);
         System.out.print("Escreva o ID do usuário que você deseja editar: ");
         Long myId = sc.nextLong();
@@ -109,9 +132,23 @@ public class agendaMainPhi {
 
                 System.out.print("Novo Telefone: ");
                 Long novoTelefone = sc.nextLong();
-                contato.setTelefone(novoTelefone);
 
-                System.out.println("Contato editado com sucesso!");
+                for(Contato i: contatos){
+                    if(i.telefones == novoTelefone){
+                        jaCadastrado = true;
+                        break;
+                    }
+                }
+
+                if(jaCadastrado){
+                    telJaCadastrado();
+                }
+                else {
+
+                    contato.setTelefone(novoTelefone);
+                    System.out.println("Contato editado com sucesso!");
+
+                }
                 Contato.printAllContacts(contatos);
                 return; // Exit the method after editing the contact
             }
@@ -119,7 +156,15 @@ public class agendaMainPhi {
         Contato.printAllContacts(contatos);
     }
 
-    static void sair() {
+    static void sair(List<Contato> contatos) throws FileNotFoundException {
+        //export(contatos);
+
+        PrintWriter out = new PrintWriter("agenda.txt");
+        for (Contato contato : contatos) {
+            out.println(contato.getId() + " | " + contato.getNome() + " " + contato.getSobrenome() + " | " + contato.getTelefone());
+        }
+        out.close();
+
         System.out.println("Você escolheu a opção 4. Você está saindo do programa. Até mais!");
         System.exit(0);
     }
@@ -127,7 +172,6 @@ public class agendaMainPhi {
     static void defOption() {
         System.out.println("Escolha outra opção.");
     }
-
 
     public static void program() {
         Scanner scan = new Scanner(System.in);
@@ -147,13 +191,13 @@ public class agendaMainPhi {
                         id++;
                         break;
                     case 2:
-                        removerContato(contatos,id);
+                        removerContato(contatos);
                         break;
                     case 3:
-                        editarContato(contatos,id);
+                        editarContato(contatos);
                         break;
                     case 4:
-                        sair();
+                        sair(contatos);
                         break;
                     default:
                         defOption();
@@ -162,6 +206,8 @@ public class agendaMainPhi {
             } catch (java.util.InputMismatchException e) {
                 System.out.println("Entrada inválida. Digite um número.");
                 scan.nextLine(); // Consume the invalid input
+            } catch (FileNotFoundException e) {
+                System.out.println("Aconteceu algo de errado...");
             }
         }
     }
