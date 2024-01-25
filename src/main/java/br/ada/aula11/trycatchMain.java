b8c2ae76adcbe302eea1178b14a00c372b448b79
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-01-24 22:17:57 -0300

    feat/ trycatchMain and numeroMain

---
+++ src/main/java/br/ada/aula11/trycatchMain.java
@@ -0,0 +1,25 @@
+package main.java.br.ada.aula11;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class trycatchMain {
+    public static void main(String[] args) throws IOException {
+        program();
+    }
+
+    public static void program() throws IOException {
+        BufferedReader Reader = null;
+        try {
+            Reader = new BufferedReader(new FileReader("Agenda.txt"));
+        }
+        catch (IOException e) {
+            System.out.println("Erro: Arquivo não existe");
+        }
+        finally{
+            System.out.println("Linha finally rodando");
+            Reader.close();
+        }
+
+    }
+}
