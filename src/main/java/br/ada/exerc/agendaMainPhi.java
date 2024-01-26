a740f4e5852b4190fef8201e39bee77436cf0fa8
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-01-26 11:54:40 -0300

    refactor/ creating comments and tidying up the code.

--- src/main/java/br/ada/exerc/agendaMainPhi.java
+++ src/main/java/br/ada/exerc/agendaMainPhi.java
@@ -1,18 +1,39 @@
-package main.java.br.ada.exerc;
+// Projeto Agenda de Contatos - Santander Coders 2023.2
+// Autor: Thiago Figueiró Ribeiro
+//
+//
+// O código abaixo simula uma agenda de contato eletrónica.
+// Nele podemos adicionar contatos, editar contatos, remover contatos,
+// e ao sair da agenda, esses contatos são exportados para um
+// arquivo "agenda.txt".
+//
+// O código está estruturado da seguinte forma:
+// * Importação de classes necessárias pra execução do código
+// * Criação da classe principal do código.
+// * * Inicialização do código
+// * * Criação da classe Contato.
+// * * * Criação dos métodos da classe Contato.
+// * * Criação das funções do código.
 
+// * Importação de classes necessárias pra execução do código * //
+package main.java.br.ada.exerc;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// * Criação da classe principal do código.
 public class agendaMainPhi {
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_RESET = "\u001B[0m";
+
+    // * * Inicialização do código
     public static void main(String[] args) {
         program();
     }
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
+    // * * Criação da classe Contato.
     public static class Contato {
         private final Long id;
         private String nome;
@@ -25,10 +46,15 @@ public class agendaMainPhi {
             this.sobreNome = sobreNome;
             this.telefones = telefones;
         }
+
+        // * * * Criação dos métodos da classe Contato.
         public Long getId() {
             return id;
         }
 
+        // ATENÇÃO: O método abaixo está em comentário já que
+        // a geração de ID é feita de forma dentro do program();
+        //
         //public void setId(long id) {
         //    this.id = id;
         //}
@@ -63,6 +89,7 @@ public class agendaMainPhi {
 
         public static void printAllContacts(List<Contato> contatos) {
             System.out.println();
+            System.out.println(">>>> Contatos <<<<");
             for (Contato contato : contatos) {
                 contato.printContato();
             }
@@ -71,13 +98,51 @@ public class agendaMainPhi {
 
     }
 
-    //public class Telefone {
-        //private Long id;
-        //private String ddd;
-        //private Long numero;
-    //}
-    public static void telJaCadastrado(){
-        System.out.println(ANSI_YELLOW + "ATENÇÃO! Operação cancelada - Telefone já cadastrado." + ANSI_RESET);
+    // * * Criação das funções do código.
+    public static void program() {
+        Scanner scan = new Scanner(System.in);
+        int opcao;
+        List<Contato> contatos = new ArrayList<>();
+        long id = 0;
+
+        while (true) {
+            exibirMenu();
+            try {
+                opcao = scan.nextInt();
+                switch (opcao) {
+                    case 1:
+                        adicionarContato(contatos, id);
+                        id++;
+                        break;
+                    case 2:
+                        removerContato(contatos);
+                        break;
+                    case 3:
+                        editarContato(contatos);
+                        break;
+                    case 4:
+                        sair(contatos);
+                        break;
+                    default:
+                        defOption();
+                        break;
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println(ANSI_YELLOW + "Entrada inválida. Digite um número." + ANSI_RESET);
+                scan.nextLine();
+            } catch (FileNotFoundException e) {
+                System.out.println(ANSI_YELLOW + "Aconteceu algo de errado..." + ANSI_RESET);
+            }
+        }
+    }
+
+    static void exibirMenu() {
+        System.out.println(">>>> Menu <<<<");
+        System.out.println("1 - Adicionar contato ");
+        System.out.println("2 - Remover contato ");
+        System.out.println("3 - Editar contato ");
+        System.out.println("4 - Sair");
+        System.out.print("Digite uma opção: ");
     }
 
     static void adicionarContato(List<Contato> contatos, long id) {
@@ -144,71 +209,32 @@ public class agendaMainPhi {
                     telJaCadastrado();
                 }
                 else {
-
                     contato.setTelefone(novoTelefone);
                     System.out.println("Contato editado com sucesso!");
-
                 }
                 Contato.printAllContacts(contatos);
-                return; // Exit the method after editing the contact
+                return;
             }
         }
         Contato.printAllContacts(contatos);
     }
 
     static void sair(List<Contato> contatos) throws FileNotFoundException {
-        //export(contatos);
-
         PrintWriter out = new PrintWriter("agenda.txt");
         for (Contato contato : contatos) {
             out.println(contato.getId() + " | " + contato.getNome() + " " + contato.getSobrenome() + " | " + contato.getTelefone());
         }
         out.close();
-
         System.out.println("Você escolheu a opção 4. Você está saindo do programa. Até mais!");
         System.exit(0);
     }
 
+    public static void telJaCadastrado(){
+        System.out.println(ANSI_YELLOW + "ATENÇÃO! Operação cancelada - Telefone já cadastrado." + ANSI_RESET);
+    }
+
     static void defOption() {
-        System.out.println("Escolha outra opção.");
+        System.out.println(ANSI_YELLOW + "Você escreveu um numero fora das opções. Escolha outra opção." + ANSI_RESET);
     }
 
-    public static void program() {
-        Scanner scan = new Scanner(System.in);
-        int opcao;
-        List<Contato> contatos = new ArrayList<>();
-        long id = 0;
-
-        while (true) {
-            System.out.println("1 - Adicionar contato, 2 - Remover contato, 3 - Editar contato, 4 - Sair");
-            System.out.print("Digite uma opção: ");
-
-            try {
-                opcao = scan.nextInt();
-                switch (opcao) {
-                    case 1:
-                        adicionarContato(contatos, id);
-                        id++;
-                        break;
-                    case 2:
-                        removerContato(contatos);
-                        break;
-                    case 3:
-                        editarContato(contatos);
-                        break;
-                    case 4:
-                        sair(contatos);
-                        break;
-                    default:
-                        defOption();
-                        break;
-                }
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("Entrada inválida. Digite um número.");
-                scan.nextLine(); // Consume the invalid input
-            } catch (FileNotFoundException e) {
-                System.out.println("Aconteceu algo de errado...");
-            }
-        }
-    }
 }
